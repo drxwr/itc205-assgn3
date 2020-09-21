10ea2efe4366329e78ee9358d63f62ed977a6a77
drxwr <dr.drew.robertson@gmail.com>
2020-09-21 21:10:39 +1000

    initial commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module BRLS {
+    exports library.borrowbook;
+    exports library.entities;
+    exports library;
+    exports library.fixbook;
+    exports library.returnbook;
+    exports library.entities.helpers;
+    exports library.payfine;
+}
